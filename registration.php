a8de23a7d3399633045f36b41f497f2d5a00ea92
andynguyen072 <andynguyen072@gmail.com>
2024-05-09 20:28:26 -0700

    Fixed forum button from not showing up?

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == TRUE)  {
-       header("Location: home.html");
+       header("Location: home.php");
     }
 ?>
 
